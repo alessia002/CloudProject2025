c376553d2ad0824dc3e131b632bfb0be13862e65
alessia002 <alessia8.bussi8@gmail.com>
2025-03-03 13:17:38 +0100

    primo commit

--- CloudProject2025/Controllers/HomeController.cs
+++ CloudProject2025/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CloudProject2025.Controllers
             return View();
         }
 
+        public IActionResult Films()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ CloudProject2025/Models/Film.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CloudProject2025.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Plot { get; set; }
+        public DateTime? Released { get; set; }
+        [Required]
+        public string? Genre{ get; set; }
+        [Required]
+        public string? Director{ get; set; }
+        public Boolean? Awards { get; set; }
+        public float? MyRate{ get; set; }
+
+
+
+        
+
+    }
+}
